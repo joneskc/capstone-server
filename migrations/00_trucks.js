2e5a3808deb18ac066181a257c8ad60a7f55c1e0
Keith Jones <keithjones@Keiths-MacBook-Pro.local>
2018-07-24 09:43:08 -0600

    added open boolean to truck tables

--- migrations/00_trucks.js
+++ migrations/00_trucks.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
       table.text('name')
       table.text('username')
       table.text('category')
+      table.boolean('open')
       table.float('latitude')
       table.float('longitude')
   })
--- seeds/00_trucks.js
+++ seeds/00_trucks.js
@@ -10,6 +10,7 @@ exports.seed = function(knex, Promise) {
           name: 'Barbed Wire Reef',
           username: 'Barbedwire',
           category: 'Tacos',
+          open: true,
           latitude: 39.758683,
           longitude: -105.007537
         },{
@@ -17,22 +18,25 @@ exports.seed = function(knex, Promise) {
           name: 'Rolling Italian',
           username: 'Italian',
           category: 'Italian',
-          latitude: 39.758683,
-          longitude: -105.007537
+          open: true,
+          latitude: 39.758837,
+          longitude: -105.006356
         },{
           id: 1000003,
           name: 'Basic Kneads Pizza',
           username: 'Basickneads',
           category: 'Pizza',
-          latitude: 39.758683,
-          longitude: -105.007537
+          open: true,
+          latitude: 39.760379,
+          longitude: -105.011399
         },{
           id: 1000004,
           name: 'Burger Chief',
           username: 'Burger',
           category: 'Burgers',
-          latitude: 39.758683,
-          longitude: -105.007537
+          open: true,
+          latitude: 39.759191,
+          longitude: -105.002859
         }
         ]);
     });
