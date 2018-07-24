5cc70d054caf1f607fcbf0d6f49362ba6b1b7d7e
Keith Jones <keithjones@Keiths-MacBook-Pro.local>
2018-07-23 19:36:27 -0600

    edited truck seeds

--- seeds/00_trucks.js
+++ seeds/00_trucks.js
@@ -8,28 +8,28 @@ exports.seed = function(knex, Promise) {
         [{
           id: 1000001,
           name: 'Barbed Wire Reef',
-          username: 'barbedwire',
+          username: 'Barbedwire',
           category: 'Tacos',
           latitude: 39.758683,
           longitude: -105.007537
         },{
           id: 1000002,
           name: 'Rolling Italian',
-          username: 'italian',
+          username: 'Italian',
           category: 'Italian',
           latitude: 39.758683,
           longitude: -105.007537
         },{
           id: 1000003,
           name: 'Basic Kneads Pizza',
-          username: 'basickneads',
+          username: 'Basickneads',
           category: 'Pizza',
           latitude: 39.758683,
           longitude: -105.007537
         },{
           id: 1000004,
           name: 'Burger Chief',
-          username: 'burger',
+          username: 'Burger',
           category: 'Burgers',
           latitude: 39.758683,
           longitude: -105.007537
