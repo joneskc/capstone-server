1ff959c6b9fddb65037528dc6b012dba8245ee67
Keith Jones <keithjones@Keiths-MacBook-Pro.local>
2018-07-25 09:28:47 -0600

    added more truck seeds to fill out map

--- seeds/00_trucks.js
+++ seeds/00_trucks.js
@@ -11,8 +11,8 @@ exports.seed = function(knex, Promise) {
           username: 'Barbedwire',
           category: 'Tacos',
           open: false,
-          latitude: 39.758683,
-          longitude: -105.007537
+          latitude: 39.758655,
+          longitude: -105.007440
         },{
           id: 1000002,
           name: 'Rolling Italian',
@@ -37,6 +37,38 @@ exports.seed = function(knex, Promise) {
           open: false,
           latitude: 39.759191,
           longitude: -105.002859
+        },{
+          id: 1000005,
+          name: 'The Biscuit Bus',
+          username: 'Biscuit',
+          category: 'Biscuits',
+          open: true,
+          latitude: 39.752448,
+          longitude: -105.014266
+        },{
+          id: 1000006,
+          name: 'Still Smokin',
+          username: 'Smokin',
+          category: 'BBQ',
+          open: true,
+          latitude: 39.761527,
+          longitude: -105.010076
+        },{
+          id: 1000007,
+          name: 'Hey PB&J',
+          username: 'PB',
+          category: 'Sandwiches',
+          open: true,
+          latitude: 39.752340,
+          longitude: -105.009064
+        },{
+          id: 1000008,
+          name: 'Ragin Cajun BBQ',
+          username: 'Cajun',
+          category: 'BBQ',
+          open: true,
+          latitude: 39.754004,
+          longitude: -104.994779
         }
         ]);
     });
